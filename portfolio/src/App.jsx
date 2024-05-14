e8123c3427df714bf95e9b349c08f46cff3d8d19
Joeeazy <joeeazy34@gmail.com>
2024-05-14 11:28:14 +0300

    Navbar section complete

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Nav from "./components/Nav";
+import Projects from "./components/Projects";
+import Services from "./components/Services";
+
+function App() {
+  return (
+    <div className="bg-slate-900">
+      <Nav />
+      {/* <About />
+      <Services />
+      <Projects />
+      <Contact />
+      <Footer /> */}
+    </div>
+  );
+}
+
+export default App;
