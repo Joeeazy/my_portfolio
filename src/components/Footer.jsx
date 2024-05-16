e51c4b6455950855e25ea2035db0de06fdd49175
Joeeazy <joeeazy34@gmail.com>
2024-05-17 02:46:57 +0300

    added an import

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import AOS from "aos";
+import "aos/dist/aos.css";
 
 import { FaGithub, FaLinkedin, FaInstagramSquare } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,4 +1,5 @@
 import AOS from "aos";
+import "aos/dist/aos.css";
 import { useEffect } from "react";
 
 export default function Services() {
